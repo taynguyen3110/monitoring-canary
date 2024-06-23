a6ebd2f43998cca907c6ea041b0a5cc544a8c735
taynguyen3110 <taynguyen3110@gmail.com>
2024-06-23 21:57:54 +1000

    First commit

---
+++ test/lambda-canary.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const LambdaCanary = require('../lib/lambda-canary-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-canary-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new LambdaCanary.LambdaCanaryStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
